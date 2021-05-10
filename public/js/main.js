432a8ccde6ff7c9bc2bdd4a8e8c24db592ccdd29
Rustam <rustam.95.ur@gmail.com>
2021-05-10 15:32:21 +0600

    Updated style

--- public/js/main.js
+++ public/js/main.js
@@ -354,7 +354,11 @@
     $('[data-countdown]').each(function () {
         var $this = $(this), finalDate = $(this).data('countdown');
         $this.countdown(finalDate, function (event) {
-            $this.html(event.strftime('<div class="single-countdown"><span class="single-countdown_time">%D</span><span class="single-countdown_text">Days</span></div><div class="single-countdown"><span class="single-countdown_time">%H</span><span class="single-countdown_text">Hours</span></div><div class="single-countdown"><span class="single-countdown_time">%M</span><span class="single-countdown_text">Min</span></div><div class="single-countdown"><span class="single-countdown_time">%S</span><span class="single-countdown_text">Sec</span></div>'));
+            $this.html(event.strftime('<div class="single-countdown"><span class="single-countdown_time">%D</span>' +
+                '<span class="single-countdown_text">дня</span></div><div class="single-countdown"><span class="single-countdown_time">%H</span>' +
+                '<span class="single-countdown_text">часов</span></div><div class="single-countdown"><span class="single-countdown_time">%M</span>' +
+                '<span class="single-countdown_text">минут</span></div><div class="single-countdown"><span class="single-countdown_time">%S</span>' +
+                '<span class="single-countdown_text">секунд</span></div>'));
         });
     });
     /*----------------------------------------*/
