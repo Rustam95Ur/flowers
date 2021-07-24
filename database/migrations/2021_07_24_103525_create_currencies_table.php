08958544e4a27cd663a77f585af9215ed9927422
Rustam <rustam.95.ur@gmail.com>
2021-07-24 21:27:30 +0600

    Added currency model

---
+++ database/migrations/2021_07_24_103525_create_currencies_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateCurrenciesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('currencies', function (Blueprint $table) {
+            $table->id();
+            $table->string('title');
+            $table->string('code');
+            $table->float('value');
+            $table->boolean('is_active');
+            $table->string('left_icon')->nullable();
+            $table->string('right_icon')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('currencies');
+    }
+}
